d8139521e828867d181b716534a7c77c51189f06
Vinícius Pommer Petto <v252160@ftad.ft.unicamp.br>
2025-08-21 20:11:21 -0300

    Organização de pastas e upload de novas atividades

---
+++ Algoritmos 2/revisao/buscador_de_bandas/buscador_de_bandas.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	typedef struct banda{
+		char nome[50], genero[50];
+		int integrantes, seg_twitter, seg_spotify, engajamento;
+	}banda;
+	
+	int i, max_engajamento=0;
+	banda vet_banda[2];
+	char genero_busca[50];
+	
+	for(i=0; i<2; i++){
+		printf("Digite o nome da banda %d: ",i+1);
+		fgets(vet_banda[i].nome, 50, stdin);
+		
+		printf("Digite o genero da banda %d: ",i+1);
+		fgets(vet_banda[i].genero, 50, stdin);
+		
+		printf("Digite os integrantes da banda %d: ",i+1);
+		scanf("%d", &vet_banda[i].integrantes);
+		
+		printf("Digite a quantidade de seguidores no Twitter da banda %d: ",i+1);
+		scanf("%d", &vet_banda[i].seg_twitter);
+		
+		printf("Digite a quantidade de ouvintes no Spotify da banda %d: ",i+1);
+		scanf("%d", &vet_banda[i].seg_spotify);
+		
+		vet_banda[i].engajamento = vet_banda[i].seg_twitter + vet_banda[i].seg_spotify;
+		
+		getchar();
+		printf("\n");
+	}
+	
+	printf("Digite o genero que deseja encontrar a banda: ");
+	fgets(genero_busca, 50, stdin);
+	
+	printf("\nBandas encontradas:\n");
+	for(i=0; i<2; i++){
+		if(strcmp(genero_busca, vet_banda[i].genero) == 0){
+			printf("%s", vet_banda[i].nome);
+		}
+	}
+	
+	for(i=0; i<2; i++){
+		if(max_engajamento < vet_banda[i].engajamento){
+			max_engajamento = vet_banda[i].engajamento;
+		}
+	}
+	
+	for(i=0; i<2; i++){
+		if(max_engajamento == vet_banda[i].engajamento){
+			printf("\nBanda com maior engajamento: %sEngajamento: %d", vet_banda[i].nome, vet_banda[i].engajamento);
+		}
+	}
+	
+	return 0;
+}
